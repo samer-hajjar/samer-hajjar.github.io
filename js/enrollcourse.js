17e817577aa2a71f3aa672adf829a5d5b6bdffa6
samer-hajjar <96047950+samer-hajjar@users.noreply.github.com>
2021-12-13 16:36:01 +0200

    ktir changes

--- js/enrollcourse.js
+++ js/enrollcourse.js
@@ -1,22 +1,18 @@
 console.log("File read successfully");
 
-function courseSendEmail() {
+function enrollCourse() {
     Email.send({
         Host: "smtp.gmail.com",
         Username: "hacklab2021@gmail.com",
         Password: "hacklab12345678",
         To: 'ahmad.ismail@publicismedia.com',
         From: "hacklab2021@gmail.com",
-        Subject: "New Course Enrollment Submitted",
-        Body: "<html><p>A new course enrollment has been submitted.</p><br></br><p style=\"display:inline\">Course: </p></html>" + document.getElementById("coursename").innerHTML +
+        Subject: "New Course Enrollement Request Submitted",
+        Body: "<html><p>A new course enrollement request has been submitted.</p><br></br><p style=\"display:inline\">Course: </p></html>" + document.getElementById("coursename").innerHTML +
             "<html><br></br><p style=\"display:inline\">Applicant name: </p></html>" + document.getElementById("formname").value +
             "<html><br></br><p style=\"display:inline\">Applicant email: </p></html>" + document.getElementById("formemail").value +
             "<html><br></br><p style=\"display:inline\">Message from applicant: </p></html>" + document.getElementById("formcomment").value,
     });
     alert("Course enrollement request sent!");
-    console.log("Course enrollement request sent!");
 };
 
-function setParameter() {
-    var param = this.alt
-}
