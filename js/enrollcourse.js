9bb8251ea2fd458b060c918c5f8a7b3f6d6d58d5
samer-hajjar <96047950+samer-hajjar@users.noreply.github.com>
2021-12-13 12:41:44 +0200

    split js pages

--- js/enrollcourse.js
+++ js/enrollcourse.js
@@ -1,21 +1,15 @@
-console.log("File read successfully");
-
-function sendEmail() {
+function courseSendEmail() {
     Email.send({
         Host: "smtp.gmail.com",
         Username: "hacklab2021@gmail.com",
         Password: "hacklab12345678",
         To: 'ahmad.ismail@publicismedia.com',
         From: "hacklab2021@gmail.com",
-        Subject: "New Job Application Submitted",
-        Body: "<html><p>A new job application has been submitted.</p><br></br><p style=\"display:inline\">Position: </p></html>" + document.getElementById("jobname").innerHTML +
+        Subject: "New Course Enrollment Submitted",
+        Body: "<html><p>A new course enrollment has been submitted.</p><br></br><p style=\"display:inline\">Course: </p></html>" + document.getElementById("coursename").innerHTML +
             "<html><br></br><p style=\"display:inline\">Applicant name: </p></html>" + document.getElementById("formname").value +
             "<html><br></br><p style=\"display:inline\">Applicant email: </p></html>" + document.getElementById("formemail").value +
             "<html><br></br><p style=\"display:inline\">Message from applicant: </p></html>" + document.getElementById("formcomment").value,
     });
-    alert("Job application sent!");
+    alert("Course enrollement sent!");
 };
-
-var jobnamekeyword = document.getElementById("jobname").innerHTML;
-var finalname = jobnamekeyword.replace(" ", "%20");
-var finaludemyname = jobnamekeyword.replace(" ", "+");
